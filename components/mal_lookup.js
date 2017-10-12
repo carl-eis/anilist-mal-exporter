e4ac5a9cf416276c4c690dec07395c514d0c2513
Carl Eiserman <carleiserman@gmail.com>
2017-10-12 11:27:04 +0200

    feature: added better config information

--- components/mal_lookup.js
+++ components/mal_lookup.js
@@ -33,16 +33,19 @@ function lookupIdES5(input, callback){
 
     req.end(function (res) {
         if (res.error) {
-
+            console.log("mal lookup: error: " + res.error);
         }
             //throw new Error(res.error);
 
         // console.log(res.body);
-        xml2js.parseString(res.body, function (err, result) {
+        xml2js.parseString(res.body, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
             let json = result;
             // console.log("PRINTING RAW: " + JSON.stringify(json, null, 4));
             if (result === undefined){
-                console.log("UNDEFINED!!!");
+                console.log("unable to fetch id!!!");
                 callback("REPLACE_ME");
             } else{
                 callback(result.anime.entry[0].id);
@@ -55,7 +58,9 @@ function createBasicAuth(){
     // console.log(new Buffer("Hello World").toString('base64'));
 
     let encString = CONFIG.MAL_USER_NAME + ":" + CONFIG.MAL_PASSWORD;
-    return "Basic " + (new Buffer(encString).toString('base64'));
+    let authString = "Basic " + (new Buffer(encString).toString('base64'));
+    console.log("debug: auth: " + authString);
+    return authString;
 }
 
 module.exports = {
